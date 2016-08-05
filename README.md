ce619e9c712bc6768304f57b5f9151410b0a54e2
Nicolás Bevacqua <nicolasbevacqua@gmail.com>
2016-08-05 13:25:02 -0300

    Simplified. Plays well with hypercwd, hot reloads.

--- README.md
+++ README.md
@@ -10,6 +10,12 @@
 
 That's it. If `config.workingDirectory` is not set, a default value of `$HOME` will be used.
 
+# info
+
+Supports hot configuration reloads. When `config.workingDirectory` changes, new tabs opened in HyperTerm will recognize the new value.
+
+This package is compatible with `hypercwd`. When `hypercwd` is installed, the configured `workingDirectory` will be used on first load and `hypercwd` will take over for tabs opened after that.
+
 # license
 
 mit
--- index.js
+++ index.js
@@ -1,35 +1,13 @@
 const untildify = require('untildify');
-const pids = new Map();
-let currentPid;
-let cwd;
 
-const pull = value => {
-  cwd = value ? untildify(value) : process.env.HOME;
-};
-const push = () => store.dispatch({ type: 'SESSION_SET_CWD', cwd });
+const pull = value => value ? untildify(value) : process.env.HOME;
+const push = cwd => store.dispatch({ type: 'SESSION_SET_CWD', cwd });
 
 exports.middleware = store => next => action => {
   const { type } = action;
 
-  if (type === 'CONFIG_LOAD') {
-    pull(action.config.workingDirectory);
-    push();
-  }
-
-  if (type === 'SESSION_ADD') {
-    pids.set(action.uid, action.pid);
-  }
-
-  if (type === 'SESSION_ADD' || type === 'SESSION_SET_ACTIVE') {
-    currentPid = pids.get(action.uid);
-  }
-
-  if (type === 'SESSION_PTY_DATA' && currentPid && currentPid === pids.get(action.uid)) {
-    push();
-  }
-
-  if (type === 'SESSION_PTY_EXIT' || type === 'SESSION_USER_EXIT') {
-    pids.delete(action.uid);
+  if (type === 'CONFIG_LOAD' || type === 'CONFIG_RELOAD') {
+    push(pull(action.config.workingDirectory));
   }
 
   next(action);
